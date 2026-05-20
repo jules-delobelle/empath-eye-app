e2648711394618d0cd0b41affa43725b6efd54d6
Jules Delobelle <jules.delobelle@edu.esiee.fr>
2026-05-20 15:04:00 +0200

    api services : login, register, getEnfant, getSessions, getDetections

---
+++ lib/services/api_services.dart
@@ -0,0 +1,75 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class ApiServices {
+  static const baseUrl = "http://10.0.2.2:8000/api";
+
+  static Future<String?> login(String username, String password) async{
+    final response = await http.post(
+      Uri.parse("$baseUrl/token/"),
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode({"username": username, "password": password})
+    );
+    if (response.statusCode == 200){
+      final data = jsonDecode(response.body);
+      return data["access"];
+    } else{
+      return null;
+    }
+  }
+
+  static Future<bool?> register(String username, String password) async{
+    final response = await http.post(
+      Uri.parse("$baseUrl/register/"),
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode({"username": username, "password": password})
+    );
+    if (response.statusCode == 201){
+      return true;
+    }
+    return false;
+  }
+
+  static Future<List?> getEnfant(String token) async{
+    final response = await http.get(
+      Uri.parse("$baseUrl/enfant/"),
+      headers: {"Content-Type": "application/json", 
+                "Authorization": "Bearer $token"}
+    );
+    if(response.statusCode == 200){
+      final data = jsonDecode(response.body);
+      return data;
+    }else{
+      return null;
+    }
+  }
+
+  static Future<List?> getSessions(String token, int enfantId) async{
+    final response = await http.get(
+      Uri.parse("$baseUrl/session/?enfant=$enfantId"),
+      headers: {"Content-Type": "application/json",
+                "Authorization": "Bearer $token"}
+    );
+    if(response.statusCode == 200){
+      final data = jsonDecode(response.body);
+      return data;
+    }else{
+      return null;
+    }
+  }
+
+  static Future<List?> getDetections(String token, int sessionId) async{
+    final response = await http.get(
+      Uri.parse("$baseUrl/detection/?session=$sessionId"),
+      headers: {"Content-Type":"application/json",
+                "Authorization": "Bearer $token"}
+    );
+    if(response.statusCode == 200){
+      final data = jsonDecode(response.body);
+      return data;
+    }else{
+      return null;
+    }
+  }
+}
+
