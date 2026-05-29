e16b83e3b4be6f2938f8b42de201546d2e8e8ee3
Jules Delobelle <jules.delobelle@edu.esiee.fr>
2026-05-29 14:18:41 +0200

    Création de l'écran Historique

--- lib/screens/history_screen.dart
+++ lib/screens/history_screen.dart
@@ -1,18 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../widgets/app_drawer.dart';
+import '../services/api_services.dart';
+import '../providers/app_provider.dart';
+import '../models/enfant.dart';
+import '../models/session.dart';
 
-class HistoryScreen extends StatelessWidget {
+class HistoryScreen extends StatefulWidget {
   const HistoryScreen({super.key});
+
+  @override
+  State<HistoryScreen> createState() => _HistoryScreenState();
+  
+}
+
+class _HistoryScreenState extends State<HistoryScreen> {
+
+  String? token;
+  List<Session> _sessions = [];
+
+  @override
+  void initState(){
+    super.initState();
+    loadHistory();
+  }
+
+  Future<void> loadHistory() async {
+    token = await ApiServices.getToken();
+    if(token != null){
+      Enfant? enfant = Provider.of<AppProvider>(context, listen: false).getEnfantSelectionne();
+      if(enfant != null) {
+        final history = await ApiServices.getSessions(token!, enfant.idEnfant);
+        if(history != null && mounted) {
+          setState(() {_sessions = history;});
+        }
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("Aucun enfant n'est sélectionné"))
+        );
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Historique")),
       drawer: AppDrawer(),
-      body: Center(
-        child: Text("History"),
+      body: ListView.builder(
+        itemCount: _sessions.length,
+        itemBuilder: (context, index) {
+          Session session = _sessions[index];
+          return ListTile(
+            title: Text("${session.date}"),
+            onTap:() => Navigator.pushNamed(context, '/session', arguments: session),
+          );
+        }
       ),
     );
-  }
 
+}
 }
