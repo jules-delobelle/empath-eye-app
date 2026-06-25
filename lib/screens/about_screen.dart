549b9c3d496a220cbe639f8c380f8cc7b8fd5c0f
chen-anais <anais.chen@edu.esiee.fr>
2026-06-26 00:09:00 +0200

    ajout des images dans la page a propos

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -35,6 +35,7 @@ class AboutScreen extends StatelessWidget {
               context,
               text: "Nous sommes une équipe de 6 élèves en troisième année d'école d'ingénieur à l'ESIEE Paris."
                     " Empath'Eye vise à aider les enfants autistes à analyser les émotions grâce à des lunettes intelligentes.",
+              imagePath: "assets/images/logo/logo_esiee.png",
               imageOnRight: true,
             ),
             const SizedBox(height: 24),
@@ -44,6 +45,7 @@ class AboutScreen extends StatelessWidget {
               context,
               text: "Nos lunettes intelligentes capturent des images à l'aide d'une caméra, "
                     "elles sont ensuite traitées par une IA et l'émotion détectée est communiquée via une LED.",
+              imagePath: "assets/images/lunettes/lunettes.jpg",
               imageOnRight: false,
             ),
             const SizedBox(height: 24),
@@ -53,6 +55,7 @@ class AboutScreen extends StatelessWidget {
               context,
               text: "L'application permet aux parents de surveiller les progrès "
                     "et à l'enfant de s'entraîner à détecter les émotions tout seul.",
+              imagePath: "assets/images/icon/icon.png",
               imageOnRight: true,
             ),
           ],
@@ -61,14 +64,21 @@ class AboutScreen extends StatelessWidget {
     );
   }
 
-  Widget _buildRow(BuildContext context, {required String text, required bool imageOnRight}) {
+  Widget _buildRow(BuildContext context, {
+    required String text,
+    required String imagePath,
+    required bool imageOnRight,
+  }) {
     final imageWidget = ClipRRect(
       borderRadius: BorderRadius.circular(8),
       child: Container(
         width: 110,
         height: 110,
         color: Colors.grey.shade300,
-        child: const Icon(Icons.image, size: 48, color: Colors.grey),
+        child: Image.asset(
+          imagePath,
+          fit: BoxFit.cover,
+        ),
       ),
     );
 
@@ -78,7 +88,7 @@ class AboutScreen extends StatelessWidget {
         text,
         style: TextStyle(
           fontSize: 16,
-          color : appColors['violet_logo'],
+          color: appColors['violet_logo'],
         ),
       ),
     );
